8fbc3a16d8611f99428b60dbee17a65d485a7b47
Brock Boutwell <brockboutwell@gmail.com>
2019-08-12 15:08:46 -0400

    added conditional logic

--- brock1.js
+++ brock1.js
@@ -1,4 +1,26 @@
-function createGreeting(){
-    console.log(my name is Brock and i am 28 years old);
+function getYearOfBirth(age){
+    if (age < 0){
+        throw new Error("age can not be a negitive number");
+    }
+    return 2019 - age;
 }
-createGreeting();
+
+function createGreeting(name, age){
+    if (name === undefined || age == undefined){
+        throw new error('Not Valid');
+    };
+    const yob = getYearOfBirth(age);
+
+    return `my name is ${name} and i am ${age} years old.
+    I was born in ${yob}`;
+}
+
+try {
+    const greeting1 = createGreeting('Brock', 28);
+    console.log(greeting1);
+
+} catch(e) {
+    console.error(e.message);
+}
+
+
