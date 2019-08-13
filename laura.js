7fb5b3b95698857d1aaaeb83261dc6a210eb1951
laura <laura.j.elias@gmail.com>
2019-08-12 19:37:39 -0500

    days of month not working, rock-paper-scissors works

--- laura.js
+++ laura.js
@@ -52,14 +52,13 @@ function decodeWord(nextWord) {
     return secretLetter;
 }
 
-
 function decode(string) {
     let stringArray = string.split(" ");
     console.log(stringArray);
+    
     let secretMessage = "";
 
     for (let i=0; i<stringArray.length; i++) {
-
         let  nextWord = stringArray[i];
         //console.log(nextWord);
         let nextLetter = decodeWord(nextWord);
@@ -71,3 +70,64 @@ function decode(string) {
 
 decode("craft block argon meter bells brown croon droop");
 decode("downy brown blunt group agate aorta bathe igloo drift death blink chest cat!! homes");
+
+
+
+
+
+function daysInMonth(month, leap) {
+    let totalDays;
+
+    switch(month, leap) {
+        case "April": 
+        case "June":
+        case "September":
+        case "November":
+            totalDays = 30;
+            break;
+        case "January":
+        case "March":
+        case "May":
+        case "July": 
+        case "August":
+        case "October":
+        case "December":            
+            totalDays = 31;
+            break;
+        case "February": console.log("February has 28 days");
+            totalDays = leap ? 29 : 28;
+            break;
+        default: console.log("That is not a valid month");
+    }
+    return `${month} has ${totalDays} days.`;
+}
+
+let answer = daysInMonth("June");
+console.log(answer);
+
+
+
+
+function rockPaperScissors(num) {
+    let randomNumber = Math.floor(Math.random() * 3) + 1;
+
+    if (num === randomNumber) {
+        return "It's a tie, try again";
+    } else if ( num === 1 && randomNumber === 2) {
+        return "Paper covers rock -- you lose.";
+    } else if (num === 1 && randomNumber === 3) {
+        return "Rock breaks scissors -- you win.";
+    } else if (num === 2 && randomNumber === 1) {
+        return "Paper covers rock -- you win.";
+    } else if (num === 2 && randomNumber === 3) {
+        return "Scissors cuts paper -- you lose.";
+    } else if (num === 3 && randomNumber === 1) {
+        return "Rock breaks scissors -- you lose.";
+    } else if  (num === 3 && randomNumber === 2) {
+        return "Scissors cuts paper -- you win.";
+    } else {
+        return "You must choose 1 (rock), 2 (paper), or 3 (scissors)";
+    }
+}
+let game = rockPaperScissors(1);
+console.log(game);
